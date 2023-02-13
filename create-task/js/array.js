32bb8c7bce9617c8e38996ec5b1709625f210d8b
AliceShi25 <shialice2006@gmail.com>
2023-02-13 13:14:44 -0500

    list

---
+++ create-task/js/array.js
@@ -0,0 +1,83 @@
+const array = [
+  {
+    name: "Abyssinian",
+    weight: "6-10 pounds",
+    colors: ["red", "blue", "fawn"],
+    lifespan: "9-15 years",
+    image:
+      "https://www.purina.com/sites/default/files/styles/kraken_generic_max_width_240/public/Abyssinian_body_7.jpg?itok=Xleasaxh",
+    funFact:
+      "Some call Abys “Cats from the Blue Nile”, believing they are the sacred cat of Egyptian Pharaohs.",
+  },
+  {
+    name: "American Bobtail",
+    weight: "7-16 pounds",
+    colors: ["white", "black", "blue"],
+    lifespan: "13-15 years",
+    image:
+      "https://www.purina.com/sites/default/files/styles/kraken_generic_max_width_240/public/AmericanBobtail_body_6.jpg?itok=974XSSws",
+    funFact:
+      "No tail is exactly the same, but the average length is 1 to 4 inches.",
+  },
+  {
+    name: "American Curl",
+    weight: "5-10 pounds",
+    colors: ["white", "black", "blue"],
+    lifespan: "13+ years",
+    image:
+      "https://www.purina.com/sites/default/files/styles/kraken_generic_max_width_240/public/AmericanCurlSHA_body_6.jpg?itok=wYcMRtpa",
+    funFact:
+      "American Curl kittens are born with straight ears that begin to curl backward after three to five days.",
+  },
+  {
+    name: "American Shorthair",
+    weight: "5-10 pounds",
+    colors: ["white", "black", "blue"],
+    lifespan: "13+ years",
+    image:
+      "https://www.purina.com/sites/default/files/styles/kraken_generic_max_width_240/public/AmericanShorthair_body_6.jpg?itok=xarDHKyX",
+    funFact:
+      "American Curl kittens are born with straight ears that begin to curl backward after three to five days.",
+  },
+  {
+    name: "American Wirehair",
+    weight: "8-12 pounds",
+    colors: ["white", "black", "blue"],
+    lifespan: "7-12 years",
+    image:
+      "https://www.purina.com/sites/default/files/styles/kraken_generic_max_width_240/public/AmericanWirehair_body_6.jpg?itok=ZP9wYuPb",
+    funFact:
+      "The gene behind the wired coat and whiskers of the American Wirehair resulted from a spontaneous natural mutation.",
+  },
+  {
+    name: "Balinese-Javanese",
+    weight: "8-16 pounds",
+    colors: ["blue", "lilac", "cream"],
+    lifespan: "15+ years",
+    image:
+      "https://www.purina.com/sites/default/files/styles/kraken_generic_max_width_240/public/Javanese_body_6.jpg?itok=8RYn4oT9",
+    funFact:
+      "Graceful dancers from the island of Bali inspired the name for this breed, which is also known for its elegant, graceful form.",
+  },
+  {
+    name: "Bengal",
+    weight: "6-18 pounds",
+    colors: ["orange", "light brown", "brown"],
+    lifespan: "12-16 years",
+    image:
+      "https://www.purina.com/sites/default/files/styles/kraken_generic_max_width_240/public/Bengal_body_6.jpg?itok=wPleMAcc",
+    funFact:
+      "Bengals are the only domestic cat with rosettes that resemble the markings on leopards, jaguars and ocelots.",
+  },
+  {
+    name: "Bengal",
+    weight: "6-18 pounds",
+    colors: ["orange", "light brown", "brown"],
+    lifespan: "12-16 years",
+    image:
+      "https://www.purina.com/sites/default/files/styles/kraken_generic_max_width_240/public/Bengal_body_6.jpg?itok=wPleMAcc",
+    funFact:
+      "Bengals are the only domestic cat with rosettes that resemble the markings on leopards, jaguars and ocelots.",
+    link: "https://www.purina.com/cats/cat-breeds?page=0",
+  },
+];
